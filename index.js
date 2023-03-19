3d2d0c2ec0516a21d5d4032b3ab449be164967f2
DreamerChaserHAH <109950820+DreamerChaserHAH@users.noreply.github.com>
2023-03-19 18:27:43 +0800

    added database connection

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 8111
+const {Client} = require("pg")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const db_client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl:true
+})
+
+const connectDb = async () => {
+    try {
+    
+        await db_client.connect()
+        //const res = await db_client.query("SELECT * FROM 'ID'")
+        //console.log(res)
+    } catch (error) {
+        console.log(error)
+    }
+}
+connectDb()
 
 var Generation = 0;
 
 var productkeys = [];
+
 
 app.get("/generate_product_key", function (req, res) {
     if(req.query.count != null){
@@ -13,34 +38,57 @@ app.get("/generate_product_key", function (req, res) {
         res.attachment('Generated_Product_Keys.txt')
         res.type('txt')
         console.log("Number of Keys generated: " + req.query.count);
-    
+        
+        var query = 'INSERT INTO "ID" (id) VALUES '
+
         for (let i = 0; i < req.query.count; i++) { 
             var new_key = makeid(20);
-            productkeys.push(new_key);
+            //productkeys.push(new_key);
             new_keys += new_key + '\n';
+            query += "('"+new_key+"')";
+            if(i == req.query.count - 1){
+                query += ";";
+            }else{
+                query += ",";
+            }
         }
+        //console.log(query);
+        db_client.query(query).then((query_result) => {
+            //console.log(query_result);
+        })
+
         res.send(new_keys);
         res.end();
     
         Generation++;
         return;
       }
-      res.end();
 });
 
-app.get("/check_product_key", function (req,res) {
+app.get("/check_product_key", async function (req,res) {
     if(req.query.value != null){
-        if(productkeys.includes(req.query.value)){
-            productkeys = productkeys.filter(function(item){
-                return item != req.query.value;
-            })
-            res.end("1");
 
-        }else{
-            res.end("0");
+        query = "SELECT * from \"ID\" where id = '"+req.query.value+"'";
+
+        try{
+            db_client.query(query).then((query_result) => {
+                console.log(query_result.rowCount);
+                if(query_result.rowCount == "1"){
+
+                    query = "DELETE FROM \"ID\" where id='"+req.query.value+"';"
+                    db_client.query(query).then((query_result) => {
+                        res.end("1")
+                    })
+                }else{
+                    res.end("0");
+                }
+            })
+        }catch(error){
+            res.end("0")
         }
+    }else{
+        res.end();
     }
-    res.end();
 });
 
 app.listen(port);
